8faf0b0ebec96069e0733b3ab68726d1e6712bcf
jldav1d <jldavid019@gmail.com>
2025-07-10 02:23:33 +0800

    config: added drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,1 +1,11 @@
 import 'dotenv/confg';
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  out: './drizzle',
+  schema: './db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});

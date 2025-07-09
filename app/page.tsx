c88b88b65556ee30801426274f3f4f20b3360059
jldav1d <jldavid019@gmail.com>
2025-07-10 02:20:23 +0800

    gitignore: ignore vscode folder

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,10 @@ export default function Home() {
   return (
     <Fragment>
       <Navbar/>
+      <div className="mx-4 flex flex-col">
+        <h2 className="text-black font-bold text-lg">Latest Blogs</h2>
+        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, esse numquam. Deserunt quasi velit amet sint quidem ullam nisi magni</p>
+      </div>
       <div className="flex flex-col md:flex-row justify-center items-center gap-4 mt-4"> 
         <BlogCard/>
         <BlogCard/>
